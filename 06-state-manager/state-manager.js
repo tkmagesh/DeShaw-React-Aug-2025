66bb7e28ecd07539f55a7254aa93366b05f147c9
Magesh Kuppan <tkmagesh77@gmail.com>
2025-08-13 12:59:41 +0530

    state manager implementation

---
+++ 06-state-manager/state-manager.js
@@ -0,0 +1,42 @@
+const StateManager = (() => {
+    let _currentState = undefined,
+        _callbackFns = [],
+        _reducerFn = undefined;
+
+    function getState(){
+        return _currentState;
+    }
+
+    function subscribe(callbackFn){
+        _callbackFns.push(callbackFn)
+    }
+
+    // private
+    function _notifyChanges(){
+        _callbackFns.forEach(callbackFn => callbackFn)
+    }
+
+    function dispatch(action){
+        let newState = _reducerFn(_currentState, action)
+        if (newState === _currentState) return ; //no state changes
+        _currentState = newState ; //update the store state
+        _notifyChanges(); //let the subscribers know that the state has changed
+    }
+
+    // factory for the store
+    function createStore(reducerFn){
+        if (typeof reducerFn !== 'function') 
+            throw new Error('a reducer is mandatory to create the store')
+        _reducerFn = reducerFn;
+        // call the reducer to initialize the currentState with a valid default state
+        return {
+            getState,
+            subscribe,
+            dispatch
+        }
+    }
+
+    return {
+        createStore
+    };
+})()
